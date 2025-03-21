74d797a14e9376dc3802bb4bf29a2462d5750dc6
Chandra shekhar Gupta <chandragupta@adobe.com>
2025-03-21 17:27:41 +0530

    eslint fixes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -16,22 +16,5 @@ export default function decorate(block) {
   
     block.textContent = '';
     block.append(bannerDiv);
-  }export default function decorate(block) {
-    const imageSrc = block.dataset.image || '';
-    const heading = block.dataset.heading || 'Default Banner Title';
-    const subheading = block.dataset.subheading || '';
+  }
   
-    const bannerDiv = document.createElement('div');
-    bannerDiv.className = 'banner';
-    bannerDiv.style.backgroundImage = `url('${imageSrc}')`;
-  
-    bannerDiv.innerHTML = `
-      <div class="banner-content">
-        <h1>${heading}</h1>
-        <div class="subheading">${subheading}</div>
-      </div>
-    `;
-  
-    block.textContent = '';
-    block.append(bannerDiv);
-  }    
